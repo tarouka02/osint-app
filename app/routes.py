2644f5b72a28f094a5972339bc03aef6fffe7663
Mohamed Trigui <mohamed.trigui279@gmail.com>
2025-04-14 19:04:09 -0500

    Add VirusTotal integration for threat analysis

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ main = Blueprint('main', __name__)
 
 IPINFO_TOKEN = "ed00c49c853ba0"
 ABUSEIPDB_KEY = "c9e3012fcd0f78b34d661a02a1dd6f9afa70a059962d31fa6a129fdd6ed98048335a4cbce25a9799"
+VIRUSTOTAL_KEY = "09ade77eb4a2776b84fa4e66a293ee33b3b013c40fff003685fa236456d0ca35"
 
 @main.route('/')
 def index():
@@ -32,4 +33,24 @@ def lookup():
     abuse_resp = requests.get(abuse_url, headers=abuse_headers)
     abuse_data = abuse_resp.json().get('data', {}) if abuse_resp.status_code == 200 else {"error": "AbuseIPDB failed"}
 
-    return render_template('result.html', ip=ip, ipinfo=ipinfo_data, abuse=abuse_data)
+
+        ### --- VirusTotal --- ###
+    vt_headers = {
+        "x-apikey": VIRUSTOTAL_KEY
+    }
+    vt_url = f"https://www.virustotal.com/api/v3/ip_addresses/{ip}"
+    vt_resp = requests.get(vt_url, headers=vt_headers)
+
+    if vt_resp.status_code == 200:
+        vt_data = vt_resp.json().get("data", {}).get("attributes", {})
+        malicious_votes = vt_data.get("last_analysis_stats", {}).get("malicious", 0)
+        harmless_votes = vt_data.get("last_analysis_stats", {}).get("harmless", 0)
+        suspicious_votes = vt_data.get("last_analysis_stats", {}).get("suspicious", 0)
+    else:
+        vt_data = {}
+        malicious_votes = harmless_votes = suspicious_votes = "?"
+        
+    return render_template('result.html', ip=ip, ipinfo=ipinfo_data,
+                    abuse=abuse_data, malicious_votes=malicious_votes,
+                    harmless_votes=harmless_votes, suspicious_votes=suspicious_votes)
+
